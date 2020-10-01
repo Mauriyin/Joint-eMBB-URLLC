891cdafb38a49960d0fead9dec182edd42165b87
Mauriyin <haoyin@uw.edu>
2020-10-01 10:52:40 -0700

    fix issues in the utility and scheduler

--- libs/scheduler.py
+++ libs/scheduler.py
@@ -1,12 +1,12 @@
 class Scheduler():
-    """Base class for scheduler.
+    """
+    Base class for scheduler.
     
     """
-
     def __init__(self, RB_map, users):
         self.RB_map = RB_map
         self.users = users
-    
+
     def allocate_resource(self):
         self._user_sort()
         raise NotImplementedError
@@ -21,16 +21,15 @@ class PfScheduler(Scheduler):
        
        Calculate the weights of each user, sort and assign one by one 
        until the rb units are all assigned, assign the embb user requesting 
-       more rbs with superiority. #TODO right???
+       more rbs with superiority. 
 
        Assign rb from the very beginning of the RB map every scheduling.
     """
-
     def __init__(self, RB_map, users):
         Scheduler.__init__(self, RB_map, users)
         self.pf_weight = []
         self.user_indexes = []
-    
+
     def allocate_resource(self):
         self._user_sort()
         rb_current = self.RB_map.rb_avi
@@ -38,27 +37,41 @@ class PfScheduler(Scheduler):
         for i in self.user_indexes:
             embb_user = self.users[i]
             if embb_user.active == 0:
-                print ("ERROR: Inactive embb user is not clear!")
+                print("ERROR: Inactive embb user is not clear!")
                 continue
             if rb_current >= embb_user.rb_num_req:
                 embb_user.rb_num_ass = embb_user.rb_num_req
-                embb_user.rate_avg = (embb_user.rate_avg * embb_user.sche_times + embb_user.DRC) / (embb_user.sche_times + 1)
+                embb_user.rate_avg = (
+                    embb_user.rate_avg * embb_user.sche_times +
+                    embb_user.DRC) / (embb_user.sche_times + 1)
                 embb_user.rate_cur = embb_user.DRC
                 embb_user.sche_times += 1
                 embb_user.rb_start = rb_start
-                self.RB_map.bitmap[rb_start:embb_user.rb_num_req+rb_start] = int(embb_user.user_info['id'])
+                self.RB_map.bitmap[rb_start:embb_user.rb_num_req +
+                                   rb_start] = int(embb_user.user_info['id'])
                 rb_start += embb_user.rb_num_ass
                 rb_current -= embb_user.rb_num_req
             else:
-                embb_user.rb_num_ass = rb_current
-                new_DRC = (rb_current * embb_user.rb_size / 1000) / embb_user.slot_len
-                embb_user.rate_avg = (embb_user.rate_avg * embb_user.sche_times + new_DRC) / (embb_user.sche_times + 1)
-                embb_user.rate_cur = new_DRC
-                embb_user.sche_times += 1
-                embb_user.rb_start = rb_start
-                self.RB_map.bitmap[rb_start:rb_current+rb_start] = int(embb_user.user_info['id'])
-                break
-        
+                if rb_current > 0:
+                    embb_user.rb_num_ass = rb_current
+                    new_DRC = (rb_current * embb_user.rb_size /
+                               1000) / embb_user.slot_len
+                    embb_user.rate_avg = (
+                        embb_user.rate_avg * embb_user.sche_times +
+                        new_DRC) / (embb_user.sche_times + 1)
+                    embb_user.rate_cur = new_DRC
+                    embb_user.sche_times += 1
+                    embb_user.rb_start = rb_start
+                    self.RB_map.bitmap[rb_start:rb_current + rb_start] = int(
+                        embb_user.user_info['id'])
+                    rb_current = 0
+                else:
+                    embb_user.rate_avg = (embb_user.rate_avg *
+                                          embb_user.sche_times) / (
+                                              embb_user.sche_times + 1)
+                    embb_user.rb_start = -1
+                    embb_user.rb_num_ass = 0
+
     def _user_sort(self):
         self.pf_weight = []
         pf_weight = []
@@ -71,5 +84,7 @@ class PfScheduler(Scheduler):
             pf_weight.append(weighti * i.DRC)
         if zero_flag is False:
             pf_weight = self.pf_weight
-        self.user_indexes = [index for index, value in sorted(list(enumerate(pf_weight)),key=lambda x:x[1],reverse = True)]
-        
+        self.user_indexes = [
+            index for index, value in sorted(
+                list(enumerate(pf_weight)), key=lambda x: x[1], reverse=True)
+        ]
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,9 +1,12 @@
 import math
 
+
 def get_embb_utility(embb_users, e_trans=0):
     embb_utility = 0.0
     for i in embb_users:
         weighti = i.DRC
         if i.sche_times:
-            embb_utility += weighti * math.log(i.rate_avg)
+            #embb_utility += weighti * math.log(i.rate_avg)
+            #embb_utility += math.log(i.rate_avg)
+            embb_utility += math.log(i.rate_cur)
     return embb_utility
